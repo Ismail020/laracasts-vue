d7dba6150de8cc7189f44369fb1f45b3222142a2
Ismail Cetin <isocetin1907@gmail.com>
2022-12-21 22:01:34 +0100

    Ep 13: Lifecycle Hooks, Fake APIs, and AJAX

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # laracasts-vue
 Laracasts course: Learn Vue 3: Step by Step
+
+npx serve
+npm install json-server --save-dev
+npx json-server db.json -p 3001
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -17,26 +17,16 @@ export default {
 
     data() {
         return {
-            assignments: [{
-                    name: 'Finish project',
-                    complete: false,
-                    id: 1,
-                    tag: 'science'
-                },
-                {
-                    name: 'Read chapter 4',
-                    complete: false,
-                    id: 2,
-                    tag: 'math'
-                },
-                {
-                    name: 'Turn in homework',
-                    complete: false,
-                    id: 3,
-                    tag: 'english'
-                }
-            ],
+            assignments: [],
         }
+    },
+
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            })
     },
 
     computed: {
