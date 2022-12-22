8a77f4ddc63371ced6c4f62ed43ca385d088200c
Ismail Cetin <isocetin1907@gmail.com>
2022-12-22 15:09:11 +0100

    Ep 18: Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+
+
+let message = $ref("Hello World");
+
+setTimeout(() => {
+    message = "Hello Vue 3";
+}, 2000);
 </script>
 
 <template>
     <main>
         <TheWelcome />
+        <p>{{ message }}</p>
+        <p>
+            <input type="text" v-model="message">
+        </p>
     </main>
 </template>
