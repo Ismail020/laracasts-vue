b34ef9c74a401f706f4512c4f6d4ad91a00587e9
Ismail Cetin <isocetin1907@gmail.com>
2022-12-24 21:56:48 +0100

    Ep 30: Teleporting

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,19 +3,15 @@ import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
 import { useTeamStore } from "@/stores/TeamStore";
-import Modal from "@/components/Modal.vue";
-import { ref } from "vue";
 
 let team = useTeamStore();
 
 team.fill();
 
-let showModal = ref(false);
-
 </script>
 
 <template>
-    <TeamHeader @add="showModal = true" />
+    <TeamHeader />
 
     <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
         <TeamMembers />
@@ -23,16 +19,5 @@ let showModal = ref(false);
 
     <TeamFooter />
 
-    <Modal :show="showModal" @close="showModal = false">
-        <template #default>
-            Need to add a new member to your team?
 
-            <form action="" class="mt-6 ">
-                <div class="flex gap-2">
-                    <input type="email" placeholder="Email Address..." class="flex-1">
-                    <button>Add</button>
-                </div>
-            </form>
-        </template>
-    </Modal>
 </template>
