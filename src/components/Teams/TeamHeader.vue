a0d2b45bffa7ee45dab06a8d54221b81a60700c5
Ismail Cetin <isocetin1907@gmail.com>
2022-12-24 21:24:51 +0100

    Ep 28: Build a Modal Component

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {useTeamStore} from "@/stores/TeamStore";
+import { useTeamStore } from "@/stores/TeamStore";
 
 let team = useTeamStore();
 </script>
@@ -8,7 +8,7 @@ let team = useTeamStore();
     <header class="flex justify-between">
         <div>
             <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-                :disabled="! team.spotsRemaining">Add Member ({{ team.spotsRemaining }} Spots
+                :disabled="!team.spotsRemaining" @click="$emit('add')">Add Member ({{ team.spotsRemaining }} Spots
                 Left)</button>
         </div>
 
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,23 +2,37 @@
 import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
-import {useTeamStore} from "@/stores/TeamStore";
+import { useTeamStore } from "@/stores/TeamStore";
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
 
 let team = useTeamStore();
 
 team.fill();
 
-setTimeout(() => {
-    team.grow(25);
-}, 2000)
+let showModal = ref(false);
+
 </script>
 
 <template>
-    <TeamHeader />
+    <TeamHeader @add="showModal = true" />
 
     <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
         <TeamMembers />
     </div>
 
     <TeamFooter />
+
+    <Modal :show="showModal" @close="showModal = false">
+        <template #default>
+            Need to add a new member to your team?
+
+            <form action="" class="mt-6 ">
+                <div class="flex gap-2">
+                    <input type="email" placeholder="Email Address..." class="flex-1">
+                    <button>Add</button>
+                </div>
+            </form>
+        </template>
+    </Modal>
 </template>
