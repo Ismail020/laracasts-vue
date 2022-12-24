ff891014841127a752e6397e004b8425d44ea465
Ismail Cetin <isocetin1907@gmail.com>
2022-12-24 21:42:13 +0100

    Ep 29: Two Ways to Transition

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,21 +5,30 @@ defineProps({
 </script>
 
 <template>
-    <div v-if="show" class="modal-mask">
-        <div class="modal-container">
-            <main>
-                <slot>
-                    Default Content
-                </slot>
-            </main>
+    <Transition
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-200"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+    >
+        <div v-if="show" class="modal-mask">
+            <div class="modal-container">
+                <main>
+                    <slot>
+                        Default Content
+                    </slot>
+                </main>
 
-            <footer class="modal-footer">
-                <slot name="footer">
-                    <button @click="$emit('close')">Close</button>
-                </slot>
-            </footer>
+                <footer class="modal-footer">
+                    <slot name="footer">
+                        <button @click="$emit('close')">Close</button>
+                    </slot>
+                </footer>
+            </div>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <style>
